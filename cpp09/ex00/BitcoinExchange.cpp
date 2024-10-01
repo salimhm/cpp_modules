6b697529770e760854b82069ce08e2375d3e52b1
Salim <salimiom@gmail.com>
2024-10-01 19:13:49 +0100

    01 done refactoring

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: shmimi <shmimi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 15:28:32 by shmimi            #+#    #+#             */
-/*   Updated: 2024/09/20 01:55:48 by shmimi           ###   ########.fr       */
+/*   Updated: 2024/10/01 19:10:28 by shmimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,4 +81,186 @@ void BitcoinExchange::setBtcData(const std::string &date, double value)
 std::map<std::string, double> BitcoinExchange::getBtcData()
 {
     return this->btcData;
+}
+
+int BitcoinExchange::parseDay(const std::string &day)
+{
+    if (day.size() != 2)
+        return 1;
+    int dayInt = std::atoi(day.c_str());
+    if (dayInt < 1 || dayInt > 31)
+        return 1;
+    return 0;
+}
+
+int BitcoinExchange::parseMonth(const std::string &month)
+{
+    if (month.size() != 2)
+        return 1;
+    int monthInt = std::atoi(month.c_str());
+    if (monthInt < 1 || monthInt > 12)
+        return 1;
+    return 0;
+}
+
+int BitcoinExchange::parseYear(const std::string &year)
+{
+    if (year.size() != 4)
+        return 1;
+    int yearInt = std::atoi(year.c_str());
+    if (yearInt < 2008 || yearInt > 2024)
+        return 1;
+    return 0;
+}
+
+int BitcoinExchange::parseValue(const std::string &value)
+{
+    if (!value.empty())
+    {
+        long valueInt = std::atol(value.c_str());
+        if (valueInt < 0)
+            return NEGATIVE;
+        else if (valueInt > 1000000)
+            return TOO_BIG;
+        else if (valueInt == 0)
+            return NOT_A_NUMBER;
+        size_t pos = 0;
+        std::stod(value, &pos);
+        if (pos != value.size())
+            return NOT_A_NUMBER;
+    }
+    return 0;
+}
+
+int BitcoinExchange::getKey(std::string &date)
+{
+    size_t counter = 0;
+    for (size_t i = 0; i < date.size(); i++)
+    {
+        if (date[i] == '-')
+            counter++;
+    }
+    if (counter != 2 || date.find("-") == std::string::npos)
+        return BAD_FORMAT;
+
+    size_t delimiterYear = date.find("-");
+    size_t delimiterMonth = date.find("-", delimiterYear + 1);
+
+    std::string year = date.substr(0, delimiterYear);
+    std::string month = date.substr(delimiterYear + 1, delimiterMonth - delimiterYear - 1);
+    std::string day = date.substr(delimiterMonth + 1);
+
+    if (parseYear(year) || parseMonth(month) || parseDay(day))
+        return BAD_FORMAT;
+    return 0;
+}
+
+std::map<std::string, double>::iterator BitcoinExchange::find_closest_date(std::string &date, std::map<std::string, double> &btcData)
+{
+    std::map<std::string, double>::iterator lower = btcData.lower_bound(date);
+
+    if (lower != btcData.end() && lower->first == date) {
+        return lower;
+    } 
+    else if (lower != btcData.begin()) {
+        --lower;
+    } 
+    return lower;
+}
+
+BitcoinExchange BitcoinExchange::load_btc_data()
+{
+    std::ifstream file("data.csv");
+    if (!file.is_open())
+    {
+        std::cerr << "Error: Couldn't open the file" << std::endl;
+        exit(1);
+    }
+    
+    std::string line;
+    std::string date;
+    std::string value;
+    
+    BitcoinExchange btc;
+    getline(file, line);
+    while(getline(file, line))
+    {
+        date = line.substr(0, line.find(","));
+        value = line.substr(line.find(",") + 1);
+        btc.setBtcData(date, std::stod(value));
+    }
+    return btc;
+}
+
+int BitcoinExchange::parseInput(const std::string &filename)
+{
+    std::ifstream file(filename);
+    if (!file.is_open())
+    {
+        std::cerr << "Error: Couldn't open the file" << std::endl;
+        return 1;
+    }
+    std::string line;
+    std::string key;
+    std::string value;
+
+    size_t delimiter;
+    
+    BitcoinExchange btc = load_btc_data();
+    std::map<std::string, double> btcData = btc.getBtcData();
+    getline(file, line);
+    delimiter = line.find("|");
+    if (delimiter == std::string::npos)
+    {
+        std::cerr << "Error: Expected date | value " << std::endl;
+        return 1;
+    }
+    else
+    {
+        key = line.substr(0, delimiter);
+        value = line.substr(delimiter + 1);
+        key = trim(key);
+        value = trim(value);
+        if (key[key.size() - 1] == ' ')
+            key = key.substr(0, key.size() - 1);
+        if (value[value.size() - 1] == ' ')
+            value = value.substr(0, value.size() - 1);
+        if (key != "date" || value != "value")
+        {
+            std::cerr << "Error: Expected date | value " << std::endl;
+            return 1;
+        }
+    }
+    if (line.empty())
+    {
+        std::cerr << "Error: Empty file" << std::endl;
+        return 1;
+    }
+    while (getline(file, line))
+    {
+        delimiter = line.find(" | ");
+        if (delimiter == std::string::npos)
+            std::cerr << "Error: bad format => " << line << std::endl;
+        else
+        {
+            key = line.substr(0, delimiter);
+            value = line.substr(delimiter + 3);
+            if (getKey(key) == BAD_FORMAT)
+                std::cerr << "Error: bad date format => " << line << std::endl;
+            else if (parseValue(value) == NEGATIVE)
+                std::cerr << "Error: not a positive number." << std::endl;
+            else if (parseValue(value) == TOO_BIG)
+                std::cerr << "Error: too large a number." << std::endl;
+            else if (parseValue(value) == NOT_A_NUMBER)
+                std::cerr << "Error: not a number." << std::endl;
+            else
+            {
+                value = trim(value);
+                std::map<std::string, double>::iterator closest = find_closest_date(key, btcData);
+                std::cout << key << " => " << value << " => " << std::stod(value) * closest->second << std::endl;
+            }
+        }
+    }
+    file.close();
+    return 0;
 }
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: shmimi <shmimi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 15:20:55 by shmimi            #+#    #+#             */
-/*   Updated: 2024/09/20 01:54:36 by shmimi           ###   ########.fr       */
+/*   Updated: 2024/10/01 19:06:51 by shmimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 #include <list>
 #include <map>
 #include <algorithm>
+#include <fstream>
+
+#define NEGATIVE 1
+#define TOO_BIG 2
+#define BAD_FORMAT 3
+#define NOT_A_NUMBER 4
 
 class BitcoinExchange
 {
@@ -29,6 +35,15 @@ class BitcoinExchange
         void setBtcData(const std::string &date, double value);
 
         std::map<std::string, double> getBtcData();
+        BitcoinExchange load_btc_data();
+        int parseInput(const std::string &filename);
+        std::map<std::string, double>::iterator find_closest_date(std::string &date, std::map<std::string, double> &btcData);
+
+        int parseDay(const std::string &day);
+        int parseMonth(const std::string &month);
+        int parseYear(const std::string &year);
+        int parseValue(const std::string &value);
+        int getKey(std::string &date);
 };
 
 
